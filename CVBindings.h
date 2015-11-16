9384606e3759dc4b483f6bd12bf04dc7d2717011
clinton <clinton.freeman@gmail.com>
2015-11-16 15:02:33 +1000

    Added blob detection bindings.

--- CVBindings.h
+++ CVBindings.h
@@ -1,14 +1,18 @@
-#ifndef _BACKGROUND_SUBTRACTOR_MOG_ACH_
-#define _BACKGROUND_SUBTRACTOR_MOG_ACH_
+#ifndef _CV_BINDINGS_ACH_
+#define _CV_BINDINGS_ACH_
 
 #ifdef __cplusplus
 	cv::Ptr<cv::BackgroundSubtractor> MOG2;
+	cv::Ptr<cv::SimpleBlobDetector> BLOB;
 extern "C" {
-
 #endif
 	void initMOG2(int history, double threshold, int detectShadows);
 
 	void applyMOG2(const CvArr* image, CvArr* fgmask);
+
+	void initBlob(int minArea, int maxArea, int minThreshold, int maxThreshold);
+
+	int detectBlobs(const CvArr* image, int* blobs);
 #ifdef __cplusplus
 }
 #endif
