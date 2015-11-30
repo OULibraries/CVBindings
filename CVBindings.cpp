a0e2b32a10e86122507118dcd2c370e8d3aaae32
clinton <clinton.freeman@gmail.com>
2015-11-30 14:09:57 +1000

    Removed unused blob detection bindings.

--- CVBindings.cpp
+++ CVBindings.cpp
@@ -16,38 +16,3 @@ extern "C" void applyMOG2(const CvArr* image, CvArr* fgmask) {
 
 	MOG2->apply(img, mask);
 }
-
-extern "C" void initBlob(int minArea, int maxArea, int minThreshold, int maxThreshold) {
-	SimpleBlobDetector::Params params;
-
-	params.minThreshold = minThreshold;
-	//params.minThreshold = maxThreshold;
-
-	//params.filterByColor = true;
-	//params.blobColor = 255;
-
-	params.filterByArea = true;
-	params.minArea = minArea;
-	params.maxArea = maxArea;
-
-	BLOB = SimpleBlobDetector::create();
-}
-
-extern "C" int detectBlobs(const CvArr* image, int* blobs) {
-	Mat img = cvarrToMat(image);
-	vector<cv::KeyPoint> detected;
-
-	BLOB->detect(img, detected);
-
-	for (int i = 0; i < detected.size(); i++) {
-		int bi = i * 3;
-
-		blobs[bi] = (int) detected.at(i).pt.x;
-		blobs[bi+1] = (int) detected.at(i).pt.y;
-		blobs[bi+2] = (int) detected.at(i).size;
-	}
-
-	return detected.size();
-}
-
-
--- CVBindings.h
+++ CVBindings.h
@@ -3,16 +3,11 @@
 
 #ifdef __cplusplus
 	cv::Ptr<cv::BackgroundSubtractor> MOG2;
-	cv::Ptr<cv::SimpleBlobDetector> BLOB;
 extern "C" {
 #endif
 	void initMOG2(int history, double threshold, int detectShadows);
 
 	void applyMOG2(const CvArr* image, CvArr* fgmask);
-
-	void initBlob(int minArea, int maxArea, int minThreshold, int maxThreshold);
-
-	int detectBlobs(const CvArr* image, int* blobs);
 #ifdef __cplusplus
 }
 #endif
